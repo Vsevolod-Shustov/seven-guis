d3a72262b1a4d44f36cf4154fae01938d8c4b17e
Vsevolod Shustov <vsevolod.shustov@gmail.com>
2026-05-12 12:12:40 +0300

    add counter with child component

---
+++ src/views/counter/CounterComponentChild.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+interface Props {
+  count?: number
+}
+const props = withDefaults(defineProps<Props>(), { count: -1 })
+defineEmits(['handleClick'])
+</script>
+
+<template>
+  <p>{{ count }}</p>
+  <button @click="$emit('handleClick')">Click Me</button>
+</template>
--- src/views/counter/CounterComponentView.vue
+++ src/views/counter/CounterComponentView.vue
@@ -1,5 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import CounterComponentChild from './CounterComponentChild.vue'
 
-<template><div>counter with component</div></template>
+const count = ref<number>(0)
+
+const sessionStorageValue = sessionStorage.getItem('componentCounterValue')
+if (sessionStorageValue !== null) {
+  count.value = Number(sessionStorageValue)
+}
+
+const handleClick = function () {
+  count.value++
+  sessionStorage.setItem('componentCounterValue', count.value.toString())
+}
+</script>
+
+<template>
+  <CounterComponentChild :count @handle-click="handleClick"></CounterComponentChild>
+</template>
 
 <style></style>
