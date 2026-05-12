74d7bd4f5049c74f02f4242e256f98a3d9553447
Vsevolod Shustov <vsevolod.shustov@gmail.com>
2026-05-12 12:29:28 +0300

    add counter with pinia and no persistence

--- src/views/counter/CounterPiniaView.vue
+++ src/views/counter/CounterPiniaView.vue
@@ -1,5 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useCounterStore } from '@/stores/counter'
+import { storeToRefs } from 'pinia'
 
-<template><div>counter with pinia</div></template>
+const store = useCounterStore()
+
+const { count } = storeToRefs(store)
+
+const handleClick = function () {
+  store.increment()
+}
+</script>
+
+<template>
+  <p>{{ count }}</p>
+  <button @click="handleClick">increase</button>
+</template>
 
 <style></style>
